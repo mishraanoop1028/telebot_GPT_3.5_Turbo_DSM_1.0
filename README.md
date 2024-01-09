ce22af1617aac20a44b319cc076f95175bc2152b
mishraanoop1028 <125237267+mishraanoop1028@users.noreply.github.com>
2024-01-09 16:24:11 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # telebot_GPT_3.5_Turbo_DSM_1.0
+## Prerequistes:
+
+-Python 3.7
+-A Telegram Account
+-An OpenAI account for API Key
+
+...
+## How to run code :
+ '''bash
+ conda create -n telebot python=3.7 -y
+ '''
+ ## How to run code :
+ '''bash
+ conda activate teelebot 
+ '''
+ 
---
+++ tele_echo_bot.py
@@ -0,0 +1,34 @@
+import logging
+from aiogram import Bot,  Dispatcher, Router, types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_TOKEN = os.getenv("TOKEN")
+# print(API_TOKEN)
+
+# configure loging
+
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start','help'])
+async def command_start_handler(message: types.Message):
+   """
+      this handler receives messages with '/start' or '/help' command
+    """
+   await message.reply("hi\nI am Echo Bot!\nPowered by aiogram.")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+   """
+      this will return echo
+    """
+   await message.answer(message.text)
+
+if __name__ == "__main__":
+   executor.start_polling(dp, skip_updates=True)
